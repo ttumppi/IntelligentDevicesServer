b5cada5798a440955448b702870426663d69b86c
tuomas lamminniemi <tuomas@lamminniemi.com>
2024-12-17 09:21:37 +0200

    Commented code

--- src/emailSender.py
+++ src/emailSender.py
@@ -8,19 +8,19 @@ class EmailSender:
         self.port = 465
         self.password = password
         self.loginEmail = loginEmail
-        self.smtpServer = "smtp.gmail.com"
-        self.context = ssl.create_default_context()
+        self.smtpServer = "smtp.gmail.com" # What email address to use
+        self.context = ssl.create_default_context() # Create encrypted space to send emails
 
        
 
     def SendEmail(self, receiverEmail, message):
          
-        msg = MIMEMultipart()
+        msg = MIMEMultipart()  # Create different parts of the email, "subject", "Sender", "Receiver"
         msg["From"] = self.loginEmail
         msg["To"] = receiverEmail
         msg["Subject"] = "Doorbell notification"
         msg.attach(MIMEText(message, "plain"))
 
-        with smtplib.SMTP_SSL("smtp.gmail.com", self.port, context=self.context) as server:
+        with smtplib.SMTP_SSL("smtp.gmail.com", self.port, context=self.context) as server: # Opens connection to email sending server tool
             server.login(self.loginEmail, self.password)
             server.sendmail(self.loginEmail, receiverEmail, msg.as_string())
